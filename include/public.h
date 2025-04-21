2112646b46698985b31fb78396c5e0af87c528b3
RedHat2003 <redhat2003@users.noreply.github.com>
2025-04-21 11:37:02 +0300

    edit the includes in public.h

--- include/public.h
+++ include/public.h
@@ -1,15 +1,14 @@
 #ifndef PUBLIC_h
 #define PUBLIC_h
 
+#include "mem.h"
 #include "object.h"
 #include "objimpl.h"
-#include "clinic/tpobject.h"
-#include "mem.h"
 #include "refcount.h"
-#include "clinic/intarrobj.h"
-#include "clinic/floatarrobj.h"
 #include "clinic/tupleobj.h"
+#include "clinic/tpobject.h"
+#include "clinic/intarrobj.h"
 #include "clinic/ndarrayobj.h"
-
+#include "clinic/floatarrobj.h"
 
 #endif
