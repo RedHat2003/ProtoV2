273dacf6da79463fc6b1cf3858d7db99fd3bae1c
RedHat2003 <redhat2003@users.noreply.github.com>
2025-04-27 23:10:32 +0300

    change flags bits and make comments much clear

--- include/clinic/ndarrayobj.h
+++ include/clinic/ndarrayobj.h
@@ -11,22 +11,19 @@
 #define FAIL 0
 #define SUCCEED 1
 
+// Individual flags: one bit per property
+#define ARRAY_C_CONTIGUOUS    0x0001  // 0000 0000 0000 0001 (bit 0)
+#define ARRAY_WRITEABLE       0x0002  // 0000 0000 0000 0010 (bit 1)
+#define ARRAY_ALIGNED         0x0004  // 0000 0000 0000 0100 (bit 2)
 
-#define ARRAY_ALIGNED         0x0100
-#define ARRAY_WRITEABLE       0x0400
+// Combined flags
+#define ARRAY_BEHAVED (ARRAY_ALIGNED | ARRAY_WRITEABLE)
 
-#define ARRAY_BEHAVED (ARRAY_ALIGNED | \
-                       ARRAY_WRITEABLE)
+// Default flags: behaved + C order
+#define ARRAY_DEFAULT (ARRAY_BEHAVED | ARRAY_C_CONTIGUOUS)
 
-#define ARRAY_C_CONTIGUOUS    0x0001
-
-
-#define ARRAY_DEFAULT (ARRAY_BEHAVED | \
-                       ARRAY_C_CONTIGUOUS) // i made the C_CON as default 
-
-// ARRAY_F_CONTIGUOUS  May be latter 
-
-
+// (Optional) later you can define Fortran order
+// #define ARRAY_F_CONTIGUOUS  0x0008  // 0000 0000 0000 1000 (bit 3)
 
 
 enum _TYPECHAR {
