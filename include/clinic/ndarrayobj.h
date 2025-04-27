3ef8e4e4e2bc104c2453a69cb433824b5773d9e4
RedHat2003 <redhat2003@users.noreply.github.com>
2025-04-27 23:11:57 +0300

    trivial change

--- include/clinic/ndarrayobj.h
+++ include/clinic/ndarrayobj.h
@@ -17,10 +17,12 @@
 #define ARRAY_ALIGNED         0x0004  // 0000 0000 0000 0100 (bit 2)
 
 // Combined flags
-#define ARRAY_BEHAVED (ARRAY_ALIGNED | ARRAY_WRITEABLE)
+#define ARRAY_BEHAVED (ARRAY_ALIGNED | \
+                       ARRAY_WRITEABLE)
 
 // Default flags: behaved + C order
-#define ARRAY_DEFAULT (ARRAY_BEHAVED | ARRAY_C_CONTIGUOUS)
+#define ARRAY_DEFAULT (ARRAY_BEHAVED | \
+                       ARRAY_C_CONTIGUOUS)
 
 // (Optional) later you can define Fortran order
 // #define ARRAY_F_CONTIGUOUS  0x0008  // 0000 0000 0000 1000 (bit 3)
